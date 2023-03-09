e1f191b310082b6639233bf99fb79ca9f9c6736a
Matheus Alan <matheusalanpv@outlook.com>
2023-03-08 21:51:51 -0300

    Criado repositórios para cada entidade.

---
+++ src/main/java/br/com/bitway/bibliotech/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package br.com.bitway.bibliotech.repository;
+
+import br.com.bitway.bibliotech.model.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+}
