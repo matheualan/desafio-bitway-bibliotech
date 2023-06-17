e501142ee22b925d7224172d1c03ebd6c37a1fef
Matheus Alan <matheusalanpv@outlook.com>
2023-06-16 23:19:23 -0300

    Projeto rodando.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into tb_clientes (nome, cpf) values ('Alan', '12312312312');
-
-insert into tb_funcionarios (nome, registro, cargo, salario) values ('Sebastião', '4562347', 'Porteiro', 1400.00);
+--insert into tb_clientes (nome, cpf) values ('Alan', '12312312312');
+--
+--insert into tb_funcionarios (nome, registro, cargo, salario) values ('Sebastião', '4562347', 'Porteiro', 1400.00);
